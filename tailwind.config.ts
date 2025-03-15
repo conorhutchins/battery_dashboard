d00fb785e317944ab616e553b7e93a535dbde84c
Conor Hutchins <conorhutchins@btinternet.com>
2025-03-15 11:54:22 +0000

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["Arial", "Helvetica", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
